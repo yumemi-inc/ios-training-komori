b900ac68d43b9b34c8def85c14ba90bdf9ff33c6
Masahiro Komori <m_komori@yumemi.co.jp>
2023-12-15 14:41:12 +0900

    first commit

---
+++ ios-training-komori/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-training-komori
+//
+//  Created by 小森 将大 on 2023/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-training-komoriUITests/ios_training_komoriUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_training_komoriUITestsLaunchTests.swift
+//  ios-training-komoriUITests
+//
+//  Created by 小森 将大 on 2023/12/15.
+//
+
+import XCTest
+
+final class ios_training_komoriUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
