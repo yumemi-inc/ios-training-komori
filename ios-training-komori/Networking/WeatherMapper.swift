cc6e914d125e9f4f364f083ff65f0e41debafb68
Masahiro Komori <m_komori@yumemi.co.jp>
2024-01-17 17:30:52 +0900

    dateEncodingStrategy・dateDecodingStrategyをiso8601に変更

--- ios-training-komori/Networking/WeatherMapper.swift
+++ ios-training-komori/Networking/WeatherMapper.swift
@@ -8,58 +8,40 @@
 import Foundation
 
 enum WeatherMapper {
-
-    private static let dateFormatter: ISO8601DateFormatter = {
-        let formatter = ISO8601DateFormatter()
-        formatter.timeZone = TimeZone.current
-        return formatter
-    }()
-
+    
     private static let encoder: JSONEncoder = {
         let encoder = JSONEncoder()
-        encoder.dateEncodingStrategy = .custom { date, encoder in
-            let formattedDate = dateFormatter.string(from: date)
-            var container = encoder.singleValueContainer()
-            try container.encode(formattedDate)
-        }
+        encoder.dateEncodingStrategy = .iso8601
         return encoder
     }()
-
+    
     private static let decoder: JSONDecoder = {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        decoder.dateDecodingStrategy = .custom { decoder in
-            let container = try decoder.singleValueContainer()
-            let dateString = try container.decode(String.self)
-            if let date = dateFormatter.date(from: dateString) {
-                return date
-            } else {
-                throw JSONError.decodeFailure
-            }
-        }
+        decoder.dateDecodingStrategy = .iso8601
         return decoder
     }()
-
+    
     static func encode(_ request: WeatherRequest) throws -> String {
         guard let data = try? encoder.encode(request),
               let json = String(data: data, encoding: .utf8)
         else {
             throw JSONError.encodeFailure
         }
-
+        
         return json
     }
-
+    
     static func decode(from json: String) throws -> WeatherResponse {
         guard let data = json.data(using: .utf8),
               let response = try? decoder.decode(WeatherResponse.self, from: data)
         else {
             throw JSONError.decodeFailure
         }
-
+        
         return response
     }
-
+    
     static func map(from response: WeatherResponse) -> Weather {
         return Weather(
             condition: response.weatherCondition,
